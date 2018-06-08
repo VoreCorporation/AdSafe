dfc3b09b967039e67883f62d579a535d8c99a3ea
DeVoresyah ArEst <de.voresyah@gmail.com>
2018-06-08 09:32:50 +0700

    Update ayo-simpen-link.js

--- assets/js/client-js/ayo-simpen-link.js
+++ assets/js/client-js/ayo-simpen-link.js
@@ -1,6 +1,6 @@
 //<![CDATA[
 var sconfig = {
-    safepage_url: "http://landing.adsafe.click/p/ayo-simpen-link.html?hash=",
+    safepage_url: "http://redirect.adsafe.click/p/ayo-simpen-link.html?hash=",
     prtoect_link: ""
 }
 
