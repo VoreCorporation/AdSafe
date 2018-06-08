9e2087e6cdbba917f92cfde18daa635ab9b850c1
DeVoresyah ArEst <de.voresyah@gmail.com>
2018-06-08 09:41:27 +0700

    Update zurokaisafelink-simpen-link.js

--- assets/js/client-js/zurokaisafelink-simpen-link.js
+++ assets/js/client-js/zurokaisafelink-simpen-link.js
@@ -1,6 +1,6 @@
 //<![CDATA[
 var sconfig = {
-    safepage_url: "http://landing.adsafe.click/p/zurokaisafelink-simpen-link.html?hash=",
+    safepage_url: "http://redirect.adsafe.click/p/zurokaisafelink-simpen-link.html?hash=",
     prtoect_link: ""
 }
 
