4fe63dd709cc28a5f156f26e4a0474fafc88b991
DeVoresyah ArEst <de.voresyah@gmail.com>
2018-06-08 09:36:19 +0700

    Update coegsekali-simpen-link.js

--- assets/js/client-js/coegsekali-simpen-link.js
+++ assets/js/client-js/coegsekali-simpen-link.js
@@ -1,6 +1,6 @@
 //<![CDATA[
 var sconfig = {
-    safepage_url: "http://landing.adsafe.click/p/coegsekali-simpen-link.html?hash=",
+    safepage_url: "http://redirect.adsafe.click/p/coegsekali-simpen-link.html?hash=",
     prtoect_link: ""
 }
 
